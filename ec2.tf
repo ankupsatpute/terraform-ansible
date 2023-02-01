5157837d7cd4cce7d2956422923f175f8e920849
ankupsatpute <95556178+ankupsatpute@users.noreply.github.com>
2023-02-01 13:17:19 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,11 @@
 resource "aws_instance" "myec2" {
-  ami                    = "ami-08df646e18b182346"
+  ami                    = "ami-01a4f99c4ac11b03c"
   instance_type          = "t2.micro"
   availability_zone = "ap-south-1a"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  key_name = "pswain"
+  key_name = "LTIDEMO"
 
   tags = {
     name = "testec2"
-  }
-
-  provisioner "local-exec" {
-    command = "echo ${aws_instance.myec2.public_ip} >> /etc/ansible/hosts"
   }
 }
