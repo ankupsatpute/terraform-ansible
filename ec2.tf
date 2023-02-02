cfe370f499ed6201f45d116893b60f34077bd2ba
ankush <ank@gmail.com>
2023-02-02 10:20:25 +0000

    add

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "ankush" {
+  ami                    = "ami-01a4f99c4ac11b03c"
+  instance_type          = "t2.micro"
+  availability_zone = "ap-south-1a"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  key_name = "LTIDEMO"
+
+  tags = {
+    Name = "Target"
+  }
+
+ 
+}
